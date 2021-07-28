29c91a11c9092e8be93390bd2c31e1ef06ae4909
Pu Wang <nicolas.weeks@gmail.com>
2021-07-28 21:07:37 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,258 @@
+//! RoCE Sanity Check
+
+#![deny(
+    // The following are allowed by default lints according to
+    // https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html
+    anonymous_parameters,
+    bare_trait_objects,
+    // box_pointers, // use box pointer to allocate on heap
+    elided_lifetimes_in_paths, // allow anonymous lifetime
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs, // TODO: add documents
+    single_use_lifetimes, // TODO: fix lifetime names only used once
+    trivial_casts, // TODO: remove trivial casts in code
+    trivial_numeric_casts,
+    // unreachable_pub, allow clippy::redundant_pub_crate lint instead
+    // unsafe_code,
+    unstable_features,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences,
+
+    warnings, // treat all wanings as errors
+
+    clippy::all,
+    clippy::restriction,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo
+)]
+#![allow(
+    // Some explicitly allowed Clippy lints, must have clear reason to allow
+    clippy::blanket_clippy_restriction_lints, // allow denying clippy::restriction directly
+    clippy::implicit_return, // actually omitting the return keyword is idiomatic Rust code
+    clippy::module_name_repetitions, // repeation of module name in a struct name is not big deal
+    clippy::multiple_crate_versions, // multi-version dependency crates is not able to fix
+    clippy::panic, // allow debug_assert, panic in production code
+    clippy::panic_in_result_fn, // allow debug_assert
+)]
+
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream, UdpSocket};
+use utilities::Cast;
+
+// ///
+// const MSG: &str = "SEND operation";
+// ///
+// const RDMAMSGR: &str = "RDMA read operation";
+// ///
+// const RDMAMSGW: &str = "RDMA write operation";
+// ///
+// const MSG_SIZE: usize = 32;
+// ///
+// const MAX_POLL_CQ_TIMEOUT: i64 = 2000;
+// ///
+// const INVALID_SIZE: isize = -1;
+
+/// The data needed to connect QP
+#[derive(Deserialize, Serialize)]
+struct CmConData {
+    /// Buffer address
+    addr: u64,
+    /// Remote key
+    rkey: u32,
+    /// QP number
+    qp_num: u32,
+    /// LID of the IB port
+    lid: u16,
+    /// gid
+    gid: u128,
+}
+
+impl CmConData {
+    ///
+    fn new() ->anyhow::Result<Self> {
+        let s = Self {
+            addr: u64::from_str_radix("55b4dec4d610", 16)?,
+            rkey: u32::from_str_radix("245", 16)?,
+            qp_num: u32::from_str_radix("11", 16)?,
+            lid: u16::from_str_radix("0", 16)?,
+            gid: u128::from_str_radix("fe80000000000000505400fffea7d042", 16)?,
+        };
+        Ok(s)
+    }
+    ///
+    const fn into_be(self) -> Self {
+        Self {
+            addr: u64::to_be(self.addr),
+            rkey: u32::to_be(self.rkey),
+            qp_num: u32::to_be(self.qp_num),
+            lid: u16::to_be(self.lid),
+            gid: u128::to_be(self.gid),
+        }
+    }
+    ///
+    const fn into_le(self) -> Self {
+        Self {
+            addr: u64::from_be(self.addr),
+            rkey: u32::from_be(self.rkey),
+            qp_num: u32::from_be(self.qp_num),
+            lid: u16::from_be(self.lid),
+            gid: u128::from_be(self.gid),
+        }
+    }
+}
+
+impl std::fmt::Display for CmConData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        //unsafe {
+        write!(
+            f,
+            "addr={:x}, rkey={:x}, qp_num={:x}, lid={:x}, gid={:x}",
+            self.addr, self.rkey, self.qp_num, self.lid, self.gid,
+        )
+        //}
+    }
+}
+
+///
+#[derive(Debug, Deserialize, Serialize)]
+enum State {
+    ///
+    ReceiveReady,
+    ///
+    SendSize(isize),
+    ///
+    ReadSize(isize),
+    ///
+    WriteSize(isize),
+}
+
+///
+#[derive(Debug)]
+pub enum TcpSock {
+    ///
+    Listener(TcpListener),
+    ///
+    Stream(TcpStream),
+}
+
+impl TcpSock {
+    ///
+    pub fn bind(port: u16) -> Self {
+        let sock_addr = format!("0.0.0.0:{}", port);
+        let tcp_listener = TcpListener::bind(&sock_addr)
+            .unwrap_or_else(|err| panic!("failed to bind to {}, the error is: {}", sock_addr, err));
+        Self::Listener(tcp_listener)
+    }
+
+    ///
+    fn listener(&self) -> &TcpListener {
+        match *self {
+            Self::Listener(ref srv) => srv,
+            Self::Stream(_) => panic!("cannot return TcpListener from TcpSock::CLIENT"),
+        }
+    }
+
+    ///
+    fn stream(&self) -> &TcpStream {
+        match *self {
+            Self::Listener(_) => panic!("cannot return TcpListener from TcpSock::CLIENT"),
+            Self::Stream(ref clnt) => clnt,
+        }
+    }
+
+    ///
+    pub fn accept(&self) -> Self {
+        match self.listener().accept() {
+            Ok((tcp_stream, addr)) => {
+                println!("new client: {:?}", addr);
+                Self::Stream(tcp_stream)
+            }
+            Err(e) => panic!("couldn't get client: {:?}", e),
+        }
+    }
+
+    ///
+    pub fn connect(server_name: &str, port: u16) -> Self {
+        let sock_addr = format!("{}:{}", server_name, port);
+        let tcp_stream = TcpStream::connect(&sock_addr).unwrap_or_else(|err| {
+            panic!("failed to connect to {}, the error is: {}", sock_addr, err)
+        });
+        Self::Stream(tcp_stream)
+    }
+
+    ///
+    fn exchange_data<T: Serialize, U: DeserializeOwned>(&self, data: &T) -> U {
+        let xfer_size = std::mem::size_of::<T>();
+        let encoded: Vec<u8> = bincode::serialize(data)
+            .unwrap_or_else(|err| panic!("failed to encode, the error is: {}", err));
+        let send_size = self
+            .stream()
+            .write(&encoded)
+            .unwrap_or_else(|err| panic!("failed to send data via socket, the error is: {}", err));
+        debug_assert_eq!(send_size, encoded.len(), "socket send data size not match");
+        let mut decode_buf = Vec::with_capacity(xfer_size);
+        unsafe {
+            decode_buf.set_len(xfer_size);
+        }
+        let recv_size = self.stream().read(&mut decode_buf).unwrap_or_else(|err| {
+            panic!("failed to receive data via socket, the error is:{}", err)
+        });
+        unsafe {
+            decode_buf.set_len(recv_size.cast());
+        }
+        debug_assert!(recv_size > 0, "failed to receive data from socket");
+
+        bincode::deserialize(&decode_buf)
+            .unwrap_or_else(|err| panic!("failed to decode, the error is: {}", err))
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let handler = std::thread::spawn(move || {
+        let socket = UdpSocket::bind("0.0.0.0:4791").unwrap_or_else(|err| panic!("failed to bind, the error is:{}", err));
+        let mut buf = [0; 64];
+        loop {
+            // Receives a single datagram message on the socket. If `buf` is too small to hold
+            // the message, it will be cut off.
+            let (amt, src) = socket.recv_from(&mut buf).unwrap_or_else(|err| panic!("failed to recv_from, the error is:{}", err));
+            println!("received {} bytes data from {}", amt, src);
+
+            // Redeclare `buf` as slice of the received data and send reverse data back to origin.
+            // let buf = &mut buf[..amt];
+            // buf.reverse();
+            // socket.send_to(buf, &src)?;
+        }
+    });
+
+    let server_name = "192.168.122.190";
+    let sock_port = 9527;
+    let client_sock = TcpSock::connect(server_name, sock_port);
+    let local_con_data = CmConData::new()?;
+    
+    println!("local connection data: {}", local_con_data);
+    let local_con_data_be = local_con_data.into_be();
+    let remote_con_data_be: CmConData = client_sock.exchange_data(&local_con_data_be);
+    let remote_con_data = remote_con_data_be.into_le();
+    println!("remote connection data: {}", remote_con_data);
+
+    // Notify server to send
+    let resp_recv_ready: State = client_sock.exchange_data(&State::ReceiveReady);
+    if let State::ReceiveReady = resp_recv_ready {
+        println!("receive ready: {:?}", resp_recv_ready);
+    } else {
+        panic!("failed to receive ready");
+    }
+
+    let join_res = handler.join();
+    if let Err(err) = join_res {
+        panic!("failed to join, the error is: {:?}", err);
+    }
+
+    Ok(())
+}
---
+++ src/roce_client.py
@@ -0,0 +1,250 @@
+import math
+import socket
+import struct
+
+from multiprocessing import Process
+from scapy.all import *
+#from scapy.contrib.roce import BTH, AETH, opcode
+from roce import AETH, AtomicAckETH, AtomicETH, BTH, ImmDt, RETH, opcode
+
+MTU = 256
+MSG_SIZE = 720
+
+DST_IP = '192.168.122.190'
+SRC_IP = '192.168.122.238'
+ROCE_PORT = 4791
+DST_PORT = 9527
+SRC_PORT = 6543
+UDP_BUF_SIZE = 2048
+
+S_VA = '0000556acaa2ea50'
+S_RKEY = '00000208'
+S_QPN = '00000011'
+S_LID = '0000'
+#S_GID = 'fe80000000000000505400fffea7d042'
+S_GID = '00000000000000000000ffffc0a87aee'
+
+ReceiveReady = 0
+SendSize = 1
+ReadSize = 2
+WriteSize = 3
+WriteImm = 4
+WriteDone = 5
+AtomicReady = 6
+AtomicDone = 7
+
+src_cpsn = 0
+src_npsn = 0
+src_epsn = 0
+
+# RoCE socket
+roce_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+roce_bind_addr = ('0.0.0.0', ROCE_PORT)
+roce_sock.bind(roce_bind_addr)
+
+# Connect to server
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_bind_addr = ('0.0.0.0', SRC_PORT)
+udp_sock.bind(client_bind_addr)
+srv_addr = (DST_IP, DST_PORT)
+udp_sock.sendto(struct.pack('c', b'1'), srv_addr)
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+print(struct.unpack('<c', exch_data))
+
+# Receive metadata
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+parsed_fields = struct.unpack('>QIIH16s', exch_data)
+dst_va, dst_rkey, dst_qpn, dst_lid, dst_gid = parsed_fields
+print(parsed_fields)
+# Send metadata
+client_metadata = S_VA + S_RKEY + S_QPN + S_LID + S_GID
+udp_sock.sendto(bytes.fromhex(client_metadata), peer_addr)
+
+# Exchange receive ready
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<i', ReceiveReady), peer_addr)
+print(struct.unpack('<i', exch_data))
+
+# Exchange send size
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<iq', SendSize, -1), peer_addr)
+parsed_fields = struct.unpack('<iq', exch_data)
+_, send_size = parsed_fields
+print(f'send_size={send_size}')
+print(parsed_fields)
+
+# RoCE send and ack
+send_pkt_num = math.ceil(send_size / MTU)
+#roce_pkts = sniff(filter=f'udp port {ROCE_PORT}', count=send_pkt_num)
+#send_req = roce_pkts[0]
+roce_pkts = []
+for i in range(send_pkt_num):
+    roce_bytes, peer_addr = roce_sock.recvfrom(UDP_BUF_SIZE)
+    send_req = BTH(roce_bytes)
+    assert send_req.psn == src_epsn + i, 'send request not match epsn'
+    roce_pkts.append(send_req)
+    send_req.show()
+ack_bth = BTH(
+    opcode = opcode('RC', 'ACKNOWLEDGE')[0],
+    psn = src_epsn + send_pkt_num - 1,
+    dqpn = dst_qpn,
+)
+ack = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/ack_bth/AETH(code='ACK', value=31, msn=1)
+ack.show()
+send(ack)
+src_epsn += send_pkt_num
+
+# Exchange read size
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<iq', ReadSize, -1), peer_addr)
+parsed_fields = struct.unpack('<iq', exch_data)
+_, read_size = parsed_fields
+print(parsed_fields)
+
+# RoCE read and ack
+read_resp_pkt_num = math.ceil(read_size / MTU)
+read_bth = BTH(
+    opcode = opcode('RC', 'RDMA_READ_REQUEST')[0],
+    psn = src_cpsn,
+    dqpn = dst_qpn,
+)
+read_reth = RETH(va=dst_va, rkey=dst_rkey, dlen=read_size)
+read_req = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/read_bth/read_reth
+read_req.show()
+send(read_req)
+src_npsn = src_cpsn + read_resp_pkt_num
+ans = []
+for i in range(read_resp_pkt_num):
+    roce_bytes, peer_addr = roce_sock.recvfrom(UDP_BUF_SIZE)
+    read_resp = BTH(roce_bytes)
+    ans.append(read_resp)
+    read_resp.show()
+#ans, unans = sr(read_req, multi=True, timeout=1)
+#assert len(ans) == 1, 'should receive 1 read response packet'
+#read_resp = ans[0].answer
+#read_resp.show()
+assert read_resp.psn == src_npsn - 1, 'read response PSN not match'
+src_cpsn = src_npsn
+
+# Exchange write size
+write_str = 'RDMA_Write_Operation'
+write_size = MSG_SIZE # len(write_str)
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<iq', WriteSize, write_size), peer_addr)
+parsed_fields = struct.unpack('<iq', exch_data)
+print(parsed_fields)
+
+# RoCE write and ack
+write_reth = RETH(va=dst_va, rkey=dst_rkey, dlen=write_size)
+if write_size <= MTU:
+    write_bth = BTH(
+        opcode = opcode('RC', 'RDMA_WRITE_ONLY')[0],
+        psn = src_cpsn,
+        dqpn = dst_qpn,
+        ackreq = True,
+    )
+    write_data = struct.pack(f'<{write_size}s', bytearray(write_str, 'ascii'))
+    write_req = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/write_bth/write_reth/Raw(load=write_data)
+    write_req.show()
+    send(write_req)
+    src_npsn = src_cpsn + 1
+else:
+    write_req_pkt_num = math.ceil(write_size / MTU)
+    write_bth = BTH(
+        opcode = opcode('RC', 'RDMA_WRITE_FIRST')[0],
+        psn = src_cpsn,
+        dqpn = dst_qpn,
+        ackreq = False,
+    )
+    write_data = struct.pack(f'<{MTU}s', bytearray(write_str, 'ascii'))
+    write_req = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/write_bth/write_reth/Raw(load=write_data)
+    write_req.show()
+    send(write_req)
+
+    write_req_mid_pkt_num = write_req_pkt_num - 2
+    for i in range(write_req_mid_pkt_num):
+        write_bth = BTH(
+            opcode = opcode('RC', 'RDMA_WRITE_MIDDLE')[0],
+            psn = src_cpsn + i + 1,
+            dqpn = dst_qpn,
+            ackreq = False,
+        )
+        write_data = struct.pack(f'<{MTU}s', bytearray(write_str, 'ascii'))
+        write_req = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/write_bth/Raw(load=write_data)
+        write_req.show()
+        send(write_req)
+
+    last_write_size = write_size % MTU
+    write_bth = BTH(
+        opcode = opcode('RC', 'RDMA_WRITE_LAST')[0],
+        psn = src_cpsn + write_req_mid_pkt_num + 1,
+        dqpn = dst_qpn,
+        ackreq = True,
+    )
+    write_data = struct.pack(f'<{last_write_size}s', bytearray(write_str, 'ascii'))
+    write_req = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/write_bth/Raw(load=write_data)
+    write_req.show()
+    send(write_req)
+    src_npsn = src_cpsn + write_req_pkt_num
+roce_bytes, peer_addr = roce_sock.recvfrom(UDP_BUF_SIZE)
+write_resp = BTH(roce_bytes)
+#write_resp = sr1(write_req, timeout=1)
+#ans, unans = sr(write_req, multi=False, timeout=1) # retry=-2
+#assert len(ans) == 1, 'should receive 1 write response packet'
+#write_resp = ans[0].answer
+write_resp.show()
+assert write_resp.psn == src_npsn - 1, 'write response PSN not match'
+src_cpsn = src_npsn
+
+# Exchange write imm
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<i', WriteImm), peer_addr)
+parsed_fields = struct.unpack('<i', exch_data)
+print(parsed_fields)
+
+# RoCE write with imm and ack
+roce_bytes, peer_addr = roce_sock.recvfrom(UDP_BUF_SIZE)
+write_imm_req = BTH(roce_bytes)
+assert write_imm_req.psn == src_epsn, 'write imm requst not match epsn'
+write_imm_req.show()
+write_imm_resp_bth = BTH(
+    opcode = opcode('RC', 'ACKNOWLEDGE')[0],
+    psn = src_epsn,
+    dqpn = dst_qpn,
+)
+write_imm_resp = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/write_imm_resp_bth/AETH(code='ACK', value=31, msn=1)
+send(write_imm_resp)
+src_epsn += 1
+
+# Exchange write done
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<i', WriteDone), peer_addr)
+parsed_fields = struct.unpack('<i', exch_data)
+print(parsed_fields)
+
+# Exchange atomic ready
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<i', AtomicReady), peer_addr)
+print(struct.unpack('<i', exch_data))
+# RoCE atomic and ack
+roce_bytes, peer_addr = roce_sock.recvfrom(UDP_BUF_SIZE)
+atomic_req = BTH(roce_bytes)
+#roce_pkts = sniff(filter=f'udp port {ROCE_PORT}', count=1)
+#atomic_req = roce_pkts[0]
+atomic_req.show()
+atomic_ack_bth = BTH(
+    opcode = opcode('RC', 'ATOMIC_ACKNOWLEDGE')[0],
+    psn = atomic_req[BTH].psn,
+    dqpn = dst_qpn,
+)
+atomic_ack = IP(dst=DST_IP)/UDP(dport=ROCE_PORT, sport=ROCE_PORT)/atomic_ack_bth/AETH(code='ACK', value=31, msn=1)/AtomicAckETH(orig=0)
+atomic_ack.show()
+send(atomic_ack)
+# Exchange atomic done
+exch_data, peer_addr = udp_sock.recvfrom(UDP_BUF_SIZE)
+udp_sock.sendto(struct.pack('<i', AtomicDone), peer_addr)
+print(struct.unpack('<i', exch_data))
+
+udp_sock.close()
+roce_sock.close()
+
