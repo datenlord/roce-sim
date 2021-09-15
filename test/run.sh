98946c0319c27f8f71f8cede43a717a5910ac7c5
Jicheng Shi <jicheng.shi@datenlord.io>
2021-09-15 14:46:16 +0800

    Adjust CI network setting

--- test/run.sh
+++ test/run.sh
@@ -8,7 +8,7 @@ DOCKER_NETWORK=mymacvlan
 HOST_IP=`hostname -I | cut -d ' ' -f 1`
 LINK_DEV_IP="10.1.1.15"
 LINK_DEV_NAME=mylink
-CONTAINER_NET="10.1.1.0/16"
+CONTAINER_NET="10.1.1.0/24"
 CONTAINER_IP="10.1.1.48"
 GRPC_PORT="9000"
 
@@ -19,7 +19,7 @@ docker network create -d macvlan --subnet=$CONTAINER_NET --ip-range=$CONTAINER_N
 sudo ip link add $LINK_DEV_NAME link eth0 type macvlan mode bridge
 sudo ip addr add $LINK_DEV_IP dev $LINK_DEV_NAME
 sudo ip link set $LINK_DEV_NAME up
-sudo ip route add $CONTAINER_IP dev $LINK_DEV_NAME
+sudo ip route add ${CONTAINER_NET} dev $LINK_DEV_NAME
 
 cd src
 #docker run --rm -d -v `pwd`:`pwd` -w `pwd` --net=mymacvlan --ip=$CONTAINER_IP --name exch_server python:3 python3 exch_server.py
@@ -37,8 +37,8 @@ cd src
 # Start Sanity Test
 cd ../test
 sleep 1 # Wait the previous test finished
-sed -i "s/SIDE_2_IP/${LINK_DEV_IP}/g" ./case.yaml
-sed -i "s/SIDE_1_IP/${HOST_IP}/g" ./case.yaml
+sed -i "s/SIDE_2_IP/${CONTAINER_IP}/g" ./case.yaml
+sed -i "s/SIDE_1_IP/${LINK_DEV_IP}/g" ./case.yaml
 sed -i "s/SIDE_1_PORT/${GRPC_PORT}/g" ./case.yaml
 sed -i "s/SIDE_2_PORT/${GRPC_PORT}/g" ./case.yaml
 
@@ -50,13 +50,13 @@ RXE_DEV=rxe_eth0
 sudo rdma link delete $RXE_DEV || true
 
 ETH_DEV=`ifconfig -s | grep '^e' | cut -d ' ' -f 1 | head -n 1`
-sudo rdma link add $RXE_DEV type rxe netdev $ETH_DEV
+sudo rdma link add $RXE_DEV type rxe netdev ${LINK_DEV_NAME}
 
 ./target/debug/sanity_side ${GRPC_PORT} 2>&1 > ./rust_side.log &
 
 # Start Python Side
 cd ./src
-docker run --rm -d -v `pwd`:`pwd` -w `pwd` --net=mymacvlan --ip=$LINK_DEV_IP --name python_side grpc-python3 python3 sanity_side.py 0.0.0.0 ${GRPC_PORT}
+docker run --rm -d -v `pwd`:`pwd` -w `pwd` --net=${DOCKER_NETWORK} --ip=${CONTAINER_IP} --name python_side grpc-python3 python3 sanity_side.py ${CONTAINER_IP} ${GRPC_PORT}
 docker logs -f python_side > ../python_side.log &
 
 # Wait a while to Start Manager Python
